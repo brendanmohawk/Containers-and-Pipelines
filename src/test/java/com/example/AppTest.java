9b1c690922afdfe09216d1fcda65265dbd7d0f75
brendanmohawk <brendan.dileo@mohawkcollege.ca>
2025-05-09 12:02:22 -0400

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -4,6 +4,7 @@ package com.example;
 // and assertEquals for checking expected results in tests.
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AppTest {
   
