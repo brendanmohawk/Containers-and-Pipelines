fe5208e281cd9b241c5202490d9a335f5390101b
brendanmohawk <brendan.dileo@mohawkcollege.ca>
2025-05-09 11:04:14 -0400

    Update varPipeline.groovy

--- jenkins/scripts/varPipeline.groovy
+++ jenkins/scripts/varPipeline.groovy
@@ -5,5 +5,20 @@
 def call(body) {
 
   def pipelineParams = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
 
+  // Define Jenkins pipeline
+  pipeline {
+    agent any
+
+    stages {
+      stage('Params') {
+        steps {
+          echo "Running with parameters: ${pipelineParams}"
+        }
+      }
+    }
+  }
 }
